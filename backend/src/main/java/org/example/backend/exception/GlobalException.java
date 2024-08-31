be28cadafa2083ad4106e90080dc0dd1473fc904
henriquemarques <henriquedms7@gmail.com>
2024-08-31 07:46:00 +0200

    add exception ExpenseNotFound and ControllerAdvice

---
+++ backend/src/main/java/org/example/backend/exception/GlobalException.java
@@ -0,0 +1,16 @@
+package org.example.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(TripNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorObject handleExpenseNotFoundException(TripNotFoundException exception){
+        return new ErrorObject(exception.getMessage());
+    }
+}
