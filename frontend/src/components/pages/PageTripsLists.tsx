4499cb073431b43020ed9c027a221efd3f5299bf
henriquemarques <henriquedms7@gmail.com>
2024-09-03 10:17:59 +0200

    adapt code from component PageTripsList

--- frontend/src/components/pages/PageTripsLists.tsx
+++ frontend/src/components/pages/PageTripsLists.tsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {Trip} from "../../model/Trip.ts";
 import TripsList from "../TripsList.tsx";
+import {getTimeGroupedTrips} from "../../util/getTimeGroupedTrips.ts";
 
 export default function PageTripsLists() {
     const [trips, setTrips] = useState<Trip[] | null>(null)
@@ -23,34 +24,17 @@ export default function PageTripsLists() {
     }, [])
 
 
-    const currentDate = new Date()
-    const pastTrips: Trip[] = []
-    const futureTrips: Trip[] = []
-    const onGoingTrips: Trip[] = []
-
-    trips?.forEach(trip => {
-        const startDate = new Date(trip.destinations[0].date)
-        const returnDate = new Date(trip.destinations[trip.destinations.length - 1].date)
-        if (startDate > currentDate) {
-            futureTrips.push(trip)
-        } else if (returnDate < currentDate) {
-            pastTrips.push(trip)
-        } else {
-            onGoingTrips.push(trip)
-        }
-    })
-
-
-
     if (!trips) {
         return (
             <h1>Loading...</h1>
         )
     }
 
+    const {pastTrips, onGoingTrips, futureTrips} = getTimeGroupedTrips(trips)
+
     return (
         <>
-            <TripsList title="On going Trips" list={onGoingTrips} />
+            <TripsList title="On going Trip" list={onGoingTrips} />
             <TripsList title="Future Trips" list={futureTrips} />
             <TripsList title="Past Trips" list={pastTrips} />
         </>
