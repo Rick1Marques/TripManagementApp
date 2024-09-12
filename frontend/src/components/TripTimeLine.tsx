f6db8c55311cf5c4a52c2c0618c28c65e6d154aa
henriquemarques <henriquedms7@gmail.com>
2024-09-12 12:40:34 +0200

    basic styling timeline

--- frontend/src/components/TripTimeLine.tsx
+++ frontend/src/components/TripTimeLine.tsx
@@ -6,7 +6,7 @@ import {
     TimelineOppositeContent,
     TimelineSeparator
 } from "@mui/lab";
-import {Box, Button, Typography} from "@mui/material";
+import {Button, Stack, Typography} from "@mui/material";
 import {getDate, getTime} from "../util/formatting.ts";
 import PlaceIcon from "@mui/icons-material/Place";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
@@ -16,6 +16,7 @@ import EventForm from "./forms/EventForm.tsx";
 import {useContext} from "react";
 import {ItineraryContext} from "../store/itinerary-context.tsx";
 import DestinationForm from "./forms/DestinationForm.tsx";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 type TimeLineProps = {
     edit?: boolean
@@ -33,55 +34,59 @@ export default function TripTimeLine({edit}: TimeLineProps) {
     }
 
     return (
-        <Timeline sx={{w: "100%"}}>
+        <Timeline sx={{width: "100%"}}>
             {dataTimeLine.map((data, index) => {
-                    return (
-                        <TimelineItem key={index}>
-                            <TimelineOppositeContent>
-                                <Box>
-                                    <Typography>{getDate(data.date)}</Typography>
-                                    <Typography>{getTime(data.date)}</Typography>
-                                </Box>
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                {data.type === "starting-point" ?
-                                    <PlaceIcon fontSize="large"/> :
-                                    data.type === "home" ?
-                                        <HomeRoundedIcon fontSize="large"/> :
-                                        data.type === "destination" ?
-                                            <AirlineStopsRoundedIcon fontSize="large"/> :
-                                            <EventIcon/>
-                                }
+                return (
+                    <TimelineItem key={index}>
+                        <TimelineOppositeContent>
+                            <Typography variant="body1">{getDate(data.date)}</Typography>
+                            <Typography variant="body2">{getTime(data.date)}</Typography>
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            {data.type === "starting-point" ?
+                                <PlaceIcon fontSize="large"/> :
+                                data.type === "home" ?
+                                    <HomeRoundedIcon fontSize="large"/> :
+                                    data.type === "destination" ?
+                                        <AirlineStopsRoundedIcon fontSize="large"/> :
+                                        <EventIcon fontSize="large"/>
+                            }
 
-                                {index !== dataTimeLine.length - 1 && <TimelineConnector/>}
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                {data.type !== "event" ?
-                                    <p>{`${data.country} - ${data.city}`}</p> :
-                                    <p>{`${data.title}`}</p>
-                                }
-                                <Box>
-                                    {(data.type !== "event" && edit) &&
-                                       <DestinationForm index={index} edit={true} destinationType={data.type}/>
-                                    }
-                                    {(data.type === "destination" && edit) &&
-                                        <Button onClick={() => handleDeleteEvent(index)}>Delete</Button>
-                                    }
-                                </Box>
+                            {index !== dataTimeLine.length - 1 && <TimelineConnector/>}
+                        </TimelineSeparator>
+                        <TimelineContent>
+                            {data.type !== "event" ?
+                                <Typography variant="body1"
+                                            paddingTop={0}>{`${data.country} - ${data.city}`}</Typography>
+                                :
+                                <Typography variant="body1">{`${data.title}`}</Typography>
+
+                            }
+
+                            {(data.type !== "event" && edit) &&
+                                <Stack maxWidth="100px" direction="row" justifyContent="space-between">
+                                    <DestinationForm index={index} edit={true} destinationType={data.type}/>
+                                    <Button variant="text"
+                                            disabled={data.type !== "destination"}
+                                            onClick={() => handleDeleteEvent(index)}><DeleteIcon/></Button>
+                                </Stack>
+                            }
+
 
                                 {(data.type === "event" && edit) &&
-                                    <Box>
-                                        <EventForm index={index}
-                                                   edit={true}
-                                                   tripEventTyped={dataTimeLine[index]}/>
-                                        <Button onClick={() => handleDeleteEvent(index)}>Delete</Button>
-                                    </Box>
-                                }
-                            </TimelineContent>
+                                <Stack maxWidth="100px" direction="row" justifyContent="space-between">
+                                    <EventForm index={index}
+                                               edit={true}
+                                               tripEventTyped={dataTimeLine[index]}/>
+                                    <Button variant="text"
+                                            onClick={() => handleDeleteEvent(index)}><DeleteIcon/></Button>
+                                </Stack>
+                            }
+                                </TimelineContent>
 
-                        </TimelineItem>
-                    )
-                }
+                                </TimelineItem>
+                                )
+                            }
             )}
         </Timeline>
     )
