663f31702ef95dbb2408ff6354e138a5db7fd510
henriquemarques <henriquedms7@gmail.com>
2024-09-16 08:04:40 +0200

    add leaflet map with markers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import PageTripsLists from "./components/pages/PageTripsLists.tsx";
 import PageTripDetail from "./components/pages/PageTripDetail.tsx";
 import PageHome from "./components/pages/PageHome.tsx";
 import ItineraryContextProvider from "./store/itinerary-context.tsx";
+import Map from "./components/pages/Map.tsx";
 
 export default function App() {
 
@@ -12,6 +13,7 @@ export default function App() {
                 <Route path="/my-trips" element={<PageTripsLists/>}/>
                 <Route path="/my-trips/:id" element={<PageTripDetail/>}/>
                 <Route path="/home" element={<PageHome/>}/>
+                <Route path="/map" element={<Map/>}/>
             </Routes>
         </ItineraryContextProvider>
     )
