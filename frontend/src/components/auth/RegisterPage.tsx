dabc51478ff587d58aa05763b52482f9ea6b2de1
henriquemarques <henriquedms7@gmail.com>
2024-09-17 09:11:07 +0200

    implement register page

---
+++ frontend/src/components/auth/RegisterPage.tsx
@@ -0,0 +1,43 @@
+import {Button, TextField} from "@mui/material";
+import {FormEvent, useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
+export default function RegisterPage() {
+    const [username, setUsername] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
+    const navigate = useNavigate()
+
+    async function register() {
+        try {
+            await axios.post("/api/users/register", {
+                username: username,
+                password: password
+            })
+            console.log("Registered successfully")
+            setUsername("")
+            setPassword("")
+            navigate("/login")
+        } catch (err) {
+            setPassword("")
+            setUsername("")
+            console.log(err)
+        }
+    }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        register();
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField required label="usename" value={username}
+                       onChange={(event) => setUsername(event.target.value)}/>
+            <TextField required type="password" label="password" value={password}
+                       onChange={(event) => setPassword(event.target.value)}/>
+            <Button type="submit">Register</Button>
+        </form>
+    )
+}
