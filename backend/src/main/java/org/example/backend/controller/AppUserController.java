306940d2aacaa89ea0234efefc7f2caf0c0201ba
henriquemarques <henriquedms7@gmail.com>
2024-09-17 11:15:37 +0200

    implement logout

--- backend/src/main/java/org/example/backend/controller/AppUserController.java
+++ backend/src/main/java/org/example/backend/controller/AppUserController.java
@@ -1,10 +1,12 @@
 package org.example.backend.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.model.AppUserRegisterDto;
 import org.example.backend.model.AppUserResponse;
 import org.example.backend.service.AppUserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,19 +17,28 @@ public class AppUserController {
     private final AppUserService appUserService;
 
     @GetMapping("/me")
-    public AppUserResponse getLoggedInUser(){
+    public AppUserResponse getLoggedInUser() {
         return appUserService.getLoggedInUser();
-    }
-
-    @PostMapping("/login")
-    public void login(){
-        // Trigger login process
     }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public AppUserResponse register(@RequestBody AppUserRegisterDto appUserRegisterDto){
+    public AppUserResponse register(@RequestBody AppUserRegisterDto appUserRegisterDto) {
         return appUserService.register(appUserRegisterDto);
     }
+    
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void login() {
+        // Trigger login process
+    }
+
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(HttpSession session) {
+        session.invalidate();
+        SecurityContextHolder.clearContext();
+    }
+
 
 }
